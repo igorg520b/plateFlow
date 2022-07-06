ca16f4ebab8127a6b48d1162777158070a88625a
Igor Gribanov <igor1215@gmail.com>
2022-07-06 18:39:22 -0230

    Update node.h

--- geometry/node.h
+++ geometry/node.h
@@ -20,6 +20,19 @@ struct icy::Node
     void Initialize(double x, double y);
     void Initialize(const Node *other);
 
+    int lsId;      // squential number in the system of equations (-1 if prescribed)
+    int locId;          // sequential number in a given floe
+    double area;        // mass that the node "represents", for applying various forces
+    double vertical_force; // for testing
+    bool isBoundary;
+
+    // initial configuration
+    Eigen::Matrix<double,3,1> x_initial;
+
+    // at step n: displacement, position, velocity, acceleration
+    Eigen::Matrix<double,5,1> un, xn, vn, an;
+    Eigen::Matrix<double,5,1> ut, xt, vt, at; // at step n+1
+
 };
 
 #endif // NODE_H
